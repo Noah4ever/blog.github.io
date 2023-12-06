29836f2a9486fed5eee408b99c2ec4a0747f2b2e
Noah <noah.thiering@web.de>
2023-12-06 12:56:25 +0100

    refactor: copy from thiering.org

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@components": "/src/components",
+      "@pages": "/src/pages",
+      "@styles": "/src/styles",
+      "@utils": "/src/utils",
+      "@types": "/src/types",
+      "@assets": "/src/assets",
+    },
+  },
+  test: {
+    environment: "jsdom",
+    reporters: ["verbose", "html"],
+    coverage: {
+      provider: "v8",
+      reporter: ["html"],
+    },
+  },
+});
