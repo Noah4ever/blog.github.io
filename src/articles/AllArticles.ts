be0428838f19332f30d560f1d49c962bc4eb33c0
Noah <noah.thiering@web.de>
2023-12-08 13:08:53 +0100

    feature: new article

--- src/articles/AllArticles.ts
+++ src/articles/AllArticles.ts
@@ -1,7 +1,8 @@
 import { Article as ArticleType } from "@/types/Article";
 
 import { article as a } from "@/articles/CreateSubdomainForGitHubPages";
+import { article as b } from "@/articles/BuildingYourOwnFreeWebsiteWithGitHubPages";
 
 export default function getAllArticles(): ArticleType[] {
-  return [a];
+  return [a, b];
 }
---
+++ src/articles/BuildingYourOwnFreeWebsiteWithGitHubPages.ts
@@ -0,0 +1,65 @@
+import authors from "@/authors/Authors";
+import { Article } from "@/types/Article";
+
+const article: Article = {
+  id: "",
+  title: "Building Your Own Free Website with GitHub Pages",
+  author: authors[0],
+  content: "",
+  tags: [
+    "GitHub Pages",
+    "Website Development",
+    "Free Hosting",
+    "Portfolio Showcase",
+    "Online Portfolio",
+  ],
+  description:
+    "Create your free website effortlessly using GitHub Pages, perfect for showcasing portfolios, blogs, and projects without any hosting costs.",
+  createdAt: "08.12.2023",
+  updatedAt: "08.12.2023",
+};
+
+const content = `
+# Unlock Your Online Presence: A Step-by-Step Guide to Free Website Hosting with GitHub Pages
+
+In today's digital age, having a personal website is a great way to showcase your portfolio, share your projects, or express your thoughts. The good news is that you can create a website for free using GitHub Pages. This step-by-step guide will walk you through the process of setting up your own free website, hosted on GitHub Pages.
+
+## Introduction
+
+GitHub Pages is a free hosting service provided by GitHub that allows you to turn your GitHub repositories into a website. It's an excellent option for hosting static websites, and you don't need to worry about server maintenance or hosting costs.
+
+## Step 1: Create a GitHub Account
+
+If you don't already have a GitHub account, start by creating one at [github.com](https://github.com/). It's free and straightforward to set up.
+
+## Step 2: Create a New Repository
+
+Once you have a GitHub account, create a new repository by clicking on the "+" icon in the top right corner of your GitHub dashboard and selecting "New repository." Give your repository a name, e.g., "<username>.github.io", where "<username>" is your GitHub username.
+
+## Step 3: Set Up GitHub Pages
+
+Inside your new repository, go to the "Settings" tab and scroll down to the "GitHub Pages" section. Choose the branch you want to use for GitHub Pages (typically "main" or "master"). GitHub will provide you with a URL for your new website, usually "<username>.github.io".
+
+## Step 4: Choose a Theme (Optional)
+
+GitHub Pages allows you to apply themes to your site easily. In the "Settings" tab of your repository, navigate to the "Theme Chooser" section to select a theme that suits your style. This step is optional, but it can give your website a polished look without any coding.
+
+## Step 5: Customize Your Website
+
+Customizing your website is where you can truly make it your own. If you have some knowledge of HTML, CSS, and Jekyll (GitHub Pages' default static site generator), you can dive into the code. Otherwise, stick to the settings and theme options provided by GitHub.
+
+## Step 6: Add Content
+
+Start adding content to your website by creating new files in your repository. You can include an "index.html" file as the homepage and create additional pages as needed. Commit your changes, and GitHub will automatically update your live site.
+
+## Conclusion
+
+Congratulations! You've just created your own free website with GitHub Pages. Whether you're building a portfolio, a blog, or a project showcase, GitHub Pages provides a straightforward and cost-free way to share your work with the world.
+
+Remember to regularly update your repository with new content and explore additional features offered by GitHub Pages to enhance your website further. Enjoy building and sharing your online presence!
+`;
+
+article.content = content;
+article.id = article.title.replace(/ /g, "-").toLowerCase();
+
+export { article };
