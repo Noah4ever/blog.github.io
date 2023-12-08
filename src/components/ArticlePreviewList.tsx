312cb7152980809ff8e1103dabc4cafc02c52703
Noah <noah.thiering@web.de>
2023-12-08 13:38:26 +0100

    feature: sort articles

--- src/components/ArticlePreviewList.tsx
+++ src/components/ArticlePreviewList.tsx
@@ -1,19 +1,32 @@
 import { Article } from "@/types/Article";
 import ArticlePreview from "./ArticlePreview";
 import "@/styles/article-preview-list.scss";
+import { useEffect, useState } from "react";
 
 export default function ArticlePreviewList({
   articles,
 }: {
   articles: Article[];
 }) {
+  const [displayedArticles, setDisplayedArticles] = useState<Article[]>([]);
+
+  useEffect(() => {
+    const sortedArticles = [...articles].sort((a, b) => {
+      return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+    });
+    setDisplayedArticles(sortedArticles);
+  }, [articles]);
+
   return (
     <div className="article-preview-list-container">
       <h2>All Articles</h2>
       <ul className="article-preview-list">
-        {articles ? (
-          articles.map((article, index) => (
-            <li key={index} className="article-preview-list-item">
+        {displayedArticles ? (
+          displayedArticles.map((article) => (
+            <li
+              key={article.id + article.createdAt}
+              className="article-preview-list-item"
+            >
               <ArticlePreview article={article} />
             </li>
           ))
