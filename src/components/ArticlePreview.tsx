d3fd7fbc3f70f31b5876b1165f045ccf6d15bef2
Noah <noah.thiering@web.de>
2023-12-12 09:59:09 +0100

    fix: open article in new tab

--- src/components/ArticlePreview.tsx
+++ src/components/ArticlePreview.tsx
@@ -1,17 +1,13 @@
 import { Article } from "@/types/Article";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "@/styles/article-preview.scss";
 
 export default function ArticlePreview({ article }: { article: Article }) {
-  const navigate = useNavigate();
-
-  function handleArticleClick() {
-    const title = article.title.replace(/\s/g, "-").toLowerCase();
-    navigate(`/article/${title}`);
-  }
-
   return (
-    <article className="article-preview-container" onClick={handleArticleClick}>
+    <Link
+      to={`/article/${article.title.replace(/\s/g, "-").toLowerCase()}`}
+      className="article-preview-container"
+    >
       <h3 className="article-preview-title">{article.title}</h3>
       <p className="article-preview-description">{article.description}</p>
       <p className="article-tags">
@@ -26,6 +22,6 @@ export default function ArticlePreview({ article }: { article: Article }) {
       <p className="article-preview-info">
         {article.author.name ?? ""}, {article.createdAt ?? ""}
       </p>
-    </article>
+    </Link>
   );
 }
